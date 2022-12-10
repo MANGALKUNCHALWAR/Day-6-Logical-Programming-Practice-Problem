c5bdbc8b8919d1976938049315a612ecefa0e11d
John Doe <mangalkunchalwar9999@gmail.com>
2022-12-10 18:37:13 +0530

    Day6 Logical Programming Practice Problem

---
+++ Main.java
@@ -0,0 +1,7 @@
+package assignment6.Day6LogicalProgrammingPracticeProblem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Logical Programming Practice Problem");
+    }
+}
